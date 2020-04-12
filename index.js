21300cacaf3b364633ea59b07843d09bab090c41
Jefreesujit <e.jefree110@gmail.com>
2020-04-12 21:10:50 +0530

    Changes to make application work with cross region request

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Max-Age', "86400");
   next();
 });
 
@@ -32,16 +33,24 @@ app.post('/shortenUrl', async (req, res) => {
     res.send({key, url});
   }).catch(e => {
     console.log('Error', e);
+    res.status(400).send(e);
   });
 });
 
 app.get('/:urlKey', async (req, res) => {
-  getRecord(req.params.urlKey).then(response => {
-    const [{ url }] = JSON.parse(response);
-    res.redirect(url);
-  }).catch(e => {
-    console.log('Error', e);
-  });
+  if (req.params.urlKey) {
+    getRecord(req.params.urlKey)
+      .then((response) => {
+        const [{ url }] = JSON.parse(response);
+        res.redirect(url);
+      })
+      .catch((e) => {
+        console.log("Error", e);
+        res.status(400).send(e);
+      });
+  } else {
+    res.status(404).end();
+  }
 });
 
 http.listen(process.env.PORT || 3009, () => {
--- src/js/main.js
+++ src/js/main.js
@@ -3,27 +3,34 @@ const el = selector => document.querySelector(selector);
 
 const getShortenedUrl = async (url) => {
   return new Promise((resolve, reject) => {
-    fetch("http://localhost:3009/shortenUrl", {
+    fetch("https://api.hurl.cf/shortenUrl", {
       method: "POST",
       body: JSON.stringify({ url }),
       headers: {
         "Content-type": "application/json; charset=UTF-8",
       },
     })
-    .then(response => response.json())
-    .then(response => resolve(response))
-    .catch(error => reject(error));
+      .then((response) => response.json())
+      .then((response) => resolve(response))
+      .catch((error) => reject(error));
   });
 }
 
 const shortenUrl = async (event) => {
   console.log(event);
   const inputUrl = el("#inputUrl").value;
-  const { url } = await getShortenedUrl(inputUrl);
-  // validate for existing input url
-  el(".output-section").classList.remove("hidden");
-  const outputUrl = el("#outputUrl");
-  outputUrl.value = url;
+  const regex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+
+  if (regex.test(inputUrl)) {
+    el(".error-section").classList.add("hidden");
+    const { url } = await getShortenedUrl(inputUrl);
+    // validate for existing input url
+    el(".output-section").classList.remove("hidden");
+    const outputUrl = el("#outputUrl");
+    outputUrl.value = url;
+  } else {
+    el(".error-section").classList.remove("hidden");
+  }
 }
 
 const copyLink = () => {
@@ -33,5 +40,5 @@ const copyLink = () => {
 }
 
 const showApiSection = () => {
-  el(".curl-code").classList.remove("hidden").select();
+  el(".curl-code").classList.remove("hidden");
 }
